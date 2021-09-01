1785e98256f16a5c641aad5e4b8d3ef624e220f8
Daniel T.Dinka <32039884+DanielDinka@users.noreply.github.com>
2021-09-01 17:01:46 +0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,27 +1,19 @@
-/*
- * File: 0-holberton.c
- * Auth: Brennan D Baraban
- *
- */
-
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - Prints "Holberton" followed by a new line.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	char *word = "Holberton";
+
+	while (*word)
+	{
+		_putchar(*word);
+		word++;
+	}
 	_putchar('\n');
 
 	return (0);
