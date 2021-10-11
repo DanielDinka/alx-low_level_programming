18ec4ab9647cc5ce60d894925a8c0025fa14ab6e
Dan Jfox <32039884+DanielDinka@users.noreply.github.com>
2021-10-11 06:16:00 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,8 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -9,4 +12,5 @@ int _putchar(char c);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
 void _puts(char *str);
+
 #endif
