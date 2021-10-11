46995ece8428e04aa7dce52b799beabe0a6fa418
Dan Jfox <32039884+DanielDinka@users.noreply.github.com>
2021-10-11 06:11:32 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,6 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
  * main - multiplies two positive numbers
@@ -8,6 +8,7 @@
  * @argv: args
  * Return: int
  */
+
 int main(int argc, char *argv[])
 {
 unsigned long mul;
